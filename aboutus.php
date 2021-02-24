84662117324fa5f8cf819534931082b760ab89a5
anishrai19 <79594023+anishrai19@users.noreply.github.com>
2021-02-24 22:04:51 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ready for sell</title>
+	<link rel="stylesheet" type="text/css" href="anish.css">
+
+</head>
+<body class="aboutus">
+	  <header>
+
+        <div class="main">
+        	  <div class="logo">
+        	  	<a href="home.php"><img src="car-logo.jpg"  id="log"></a>
+        	 <ul>
+        	 	 <li><a href="home.php">home</a></li>
+        	 	  <li><a href="login.php">login</a></li>
+        	 	  <li><a href="cardetails.php">car details</a></li>
+        	 	  <li><a href="#">about us</a></li>
+        	 	  <li><a href="contactus.php">contact us</a></li>
+
+        </div>
+    </header>
+     <div id="outer">
+     <div id="sir">
+           <div class="pt"><image src="qq.jpg" style="width: 150px; height: 150px;" class="s"/></div>
+            <h3 id="an" style="margin-top:14px; text-align:center;">WEBSITE DESIGNER</h3><h4 style="margin-top:14px; text-align:center;">Anish Kumar Rai</h4>
+         <h5 style="margin-top:14px; text-align:center; font-family: times new roman;">MCA 1st Yr from DYPMIR,Pimpri</h5>
+         
+         </div>
+         <div class="in" style="width: 900px;height: 370px; margin-left:200px;margin-top: 60px;"><br>
+            <h6 style="text-align:center; font-size: 30px;color: red;"> <b><u>ABOUT US</u></b></h6><br>
+         <h7 style="margin-top:20px;padding-left: 20px; font-family: time new roman;font-size: 22px;">This website is mainly design for the customers to purchase the vehicle at resonable cost in their area. My idea behind this website to produce full knowledge of cars to whom customer want to perchase.<br>
+         The right kind of a car dealer website accentuates the odds of finding success for an agency.We make sure that we come with a high performance cars dealer website solution,irrespective of wheather you are looking for car dealer inventory website or best used car dealer website.<br>
+         Car dealer website that we create are a sale centric solution. They feature an interactive sales panel,exclusive mobile application support,new car inventory and a dealership panel with various report. </h7>   
+                     </div>
+                 
+                    
+    </body>
+</html>
---
+++ audi.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ready for sell</title>
+	<link rel="stylesheet" type="text/css" href="anish.css">
+
+</head>
+<body class="cardet">
+	  <header>
+
+        <div class="main">
+        	  <div class="logo">
+        	  	<a href="home.php"><img src="car-logo.jpg" id="log"></a>
+        	 <ul>
+        	 	
+        	 	  <li><a href="cardetails.php">Back to menu</a></li>
+        	 	 
+            </ul>
+        </div>
+    </div>
+    </header>
+    <div class="menuall">
+            <h2 class="menuhead">Collection of<span> AUDI CARS</span></h2>
+            <p id="serve">We deliver best car in best price to our Customer</p>
+            <div class="container-box">
+                <div class="singleImg">
+               <img src="audiA3.png">
+                <div class="item">
+                <h1>Audi-A3</h1>
+            </div>
+            </div>
+
+            
+                <div class="singleImg">
+               <img src="audiA4.png">
+                <div class="item">
+                <h1>Audi-A4</h1>
+            </div>
+            </div>
+
+        
+                <div class="singleImg">
+               <img src="audi-A6.png">
+                <div class="item">
+                <h1>Audi-A6</h1>
+            </div>
+            </div>
+
+        
+                <div class="singleImg">
+               <img src="audi-q3.png">
+                <div class="item">
+                <h1>Audi-Q3</h1>
+            </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
